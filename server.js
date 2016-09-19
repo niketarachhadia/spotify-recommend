99efc076cc4aeee2e9065b0d28dd8acc088ecb1d
niketarachhadia <niketa.rachhadia@gmail.com>
2016-09-19 17:53:13 -0500

    related artist implememted

---
+++ server.js
@@ -0,0 +1,50 @@
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
+var app = express();
+app.use(express.static('public'));
+
+app.get('/search/:name', function(req, res) {
+    var searchReq = getFromApi('search', {
+        q: req.params.name,
+        limit: 1,
+        type: 'artist'
+    });
+
+    searchReq.on('end', function(item) {
+        var artist = item.artists.items[0];
+	var id=artist.id;
+	var relatedArtists = getFromApi('artists/'+id+'/related-artists');
+	relatedArtists.on('end',function(data){
+		artist.related=data.artists; 
+		res.json(artist);
+	
+	});
+	relatedArtists.on('error', function(code){
+		 res.sendStatus(code);
+	});
+    	
+    });
+
+    searchReq.on('error', function(code) {
+        res.sendStatus(code);
+    });
+});
+
+var getFromApi = function(endpoint, args) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/' + endpoint)
+           .qs(args)
+           .end(function(response) {
+                if (response.ok) {
+                    emitter.emit('end', response.body);
+                }
+                else {
+                    emitter.emit('error', response.code);
+                }
+            });
+    return emitter;
+};
+
+app.listen(process.env.PORT || 8089);
+
